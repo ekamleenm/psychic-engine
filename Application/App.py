56ea1511dbd39c15d5850b89cf1bac437eac4ca7
ekamleen <ekamleenm@gmail.com>
2024-04-23 23:33:29 -0700

    second menu added

--- Application/App.py
+++ Application/App.py
@@ -64,9 +64,15 @@ class NLPApp:
         else:
             print('Not registered')
 
+    # this is hitting API model
     def __ner(self):
         para = input('enter the sentences: ')
         search_term = input('enter the search term: ')
+        client = nlpcloud.Client('finetuned-gpt-neox-20b', '2be48590f743cc9181e20cb9c62cc2979d8646b1', gpu=True,
+                                 lang='en'
+                                 )
+        response = client.entities(para, searched_entity='search_term')
+        print(response)
 
     def __lang_detection(self):
         pass
